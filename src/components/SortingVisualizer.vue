19ad5f4403bfe5f2dd52ef9c230292e6bcca8916
Saiat Kalbiev <kalbievich11@gmail.com>
2019-10-26 14:06:46 +0400

    Implement bubble sort full animation

--- src/components/SortingVisualizer.vue
+++ src/components/SortingVisualizer.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="sort-visualizer">
-
     <div class="buttons-placeholder">
       <button @click="resetArray">Reset</button>
       <button @click="bubbleSort">Run Bubble Sort</button>
     </div>
-
     <div class="bar-container">
       <div class="bar"
            v-for="(el, index) in array"
            :style="{height: `${el * 0.7}px`}"
+           :id="`bar-${index}`"
            :key="index">
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
+  import BubbleSort from "../services/BubbleSort";
+
+  const MAIN_BG_COLOR = 'chartreuse';
+  const SECONDARY_BG_COLOR = 'red';
+
   export default {
     name: 'SortingVisualizer',
     data() {
@@ -29,78 +32,77 @@
       resetArray() {
         const tmpArr = [];
         for (let i = 0; i < 250; i++) {
-          tmpArr.push(randomIntFromInterval(5, 1000));
+          tmpArr.push(this.randomIntFromInterval(5, 1000));
         }
         this.array = [...tmpArr];
       },
+      randomIntFromInterval(max, min) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      },
+      changeBarColor(color, index) {
+        document.getElementById(`bar-${index}`).style.backgroundColor = color;
+      },
+      resetBackgroundColors() {
+        document.querySelector('.bar-container').children.forEach((child) => {
+          child.style.backgroundColor = MAIN_BG_COLOR;
+        })
+      },
       bubbleSort() {
-        console.log(document);
-
-        let isSorted = false;
-        let index = this.array.length - 1;
-
-        while(!isSorted) {
-          isSorted = true;
-          for (let i = 0; i < index; i++) {
-            isSorted = false;
+        BubbleSort.$on('onItemSwap', (data) => {
+          setTimeout(() => {
+            this.resetBackgroundColors();
+            this.changeBarColor(SECONDARY_BG_COLOR, data.second.index);
+            this.array[data.first.index] = data.first.value;
+            this.array[data.second.index] = data.second.value;
+            this.array = [...this.array];
             setTimeout(() => {
-              if (this.array[i] > this.array[i + 1]) {
-                let tmp = this.array[i];
-                this.array[i] = this.array[i + 1];
-                this.array[i + 1] = tmp;
-                this.array = [...this.array];
-              }
-            }, 100)
-          }
-
-          index--;
-        }
+              this.resetBackgroundColors();
+            }, 5)
+          }, data.counter / 10)
+        });
+        BubbleSort.run(this.array);
       }
     },
     beforeMount() {
       this.resetArray();
     }
   }
-
-  function randomIntFromInterval(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
 </script>
 
 <style lang="scss" scoped>
 
-  .buttons-placeholder {
-    margin: auto;
-    width: 1500px;
-    padding-bottom: 20px;
-    padding-top: 10px;
-    border-bottom: 1px solid #cecece;
+  .sort-visualizer {
+    .buttons-placeholder {
+      margin: auto;
+      width: 1500px;
+      padding-bottom: 20px;
+      padding-top: 10px;
+      border-bottom: 1px solid #cecece;
 
-    button {
-      margin-right: 10px;
-      &:hover {
-        cursor: pointer;
+      button {
+        margin-right: 10px;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+
+    .bar-container {
+      margin: auto;
+      margin-top: 60px;
+      border-bottom: 1px solid #cecece;
+      width: 1500px;
+      text-align: center;
+
+      .bar {
+        margin-right: 2px;
+        font-size: 0;
+        width: 4px;
+        height: 2px;
+        display: inline-block;
+        background-color: chartreuse;
       }
     }
   }
-
-  .bar-container {
-    margin: auto;
-    margin-top: 60px;
-    border-bottom: 1px solid #cecece;
-    width: 1500px;
-    text-align: center;
-
-    .bar {
-      margin-right: 2px;
-      font-size: 0;
-      width: 4px;
-      height: 2px;
-      display: inline-block;
-      background-color: #782b64;
-    }
-
-  }
-
 </style>
