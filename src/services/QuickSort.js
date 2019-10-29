c9fa1fbfa919cdfacbc2f051c12b3bc37cafc905
Saiat Kalbiev <kalbievich11@gmail.com>
2019-10-29 16:40:06 +0400

    Implement quick sort algorithm helper service

---
+++ src/services/QuickSort.js
@@ -0,0 +1,53 @@
+import Vue from "vue";
+
+export default new Vue({
+  methods: {
+    run(arr) {
+      this.quickSort(arr, 0, arr.length - 1)
+    },
+    quickSort(arr, left, right) {
+      // If somehow the left are equal or if the data is passed wrongly
+      if (left >= right) {
+        return false;
+      }
+
+      // Getting approximate middle of the array
+      let pivot = arr[Math.floor((left + right) / 2)];
+
+      let index = this.partition(arr, left, right, pivot);
+
+      this.quickSort(arr, left, index - 1);
+      this.quickSort(arr, index, right);
+    },
+    partition(arr, left, right, pivot) {
+      while (left <= right) {
+
+        // Moving left index to right
+        while (arr[left] < pivot) {
+          left++;
+        }
+
+        // Moving right index to left
+        while (arr[right] > pivot) {
+          right--;
+        }
+
+        // If left is less than right swap their places
+        if (left <= right) {
+          let tmp = arr[left];
+          arr[left] = arr[right];
+          arr[right] = tmp;
+          left++;
+          right--;
+
+          this.$emit('onValueSwap', {
+            arr: [...arr],
+            left,
+            right
+          });
+        }
+      }
+      return left;
+    }
+  }
+});
